2a3a1bb77fe9d2f6a1144eff76957934ca321707
Chris Roth <c.roth.lambda@gmail.com>
2018-12-08 13:44:09 -0600

    Actions Calls: Completed
    MVP completed

--- index.js
+++ index.js
@@ -78,6 +78,83 @@ server.delete('/projects/remove/:id', (req, res)=>{
 
 })
 
+server.get('/project/:id/actions', (req, res)=>{
+    const { id } = req.params;
+    projectDB.getProjectActions(id)
+        .then(actions=>{
+            res.json(actions)
+        })
+        .catch(err=>{
+            res.status(500).json({message: "issue with your request"})
+        })
+
+
+})
+
+//actions
+server.get('/actions', (req, res)=>{
+    actionDB.get()
+        .then(actions=>{
+            res.json(actions)
+        })
+        .catch(err=>{
+            res.status(500).json({message: "problem with request"})
+        })
+})
+
+server.get('/actions/:id', (req, res)=>{
+    const { id } = req.params;
+    actionDB.get(id)
+        .then(action=>{
+            res.json(action)
+        })
+        .catch(err=>{
+            res.status(500).json({message: "problem with request"})
+        })
+})
+
+server.post('/actions/add', (req, res)=>{
+    const data = req.body;
+    actionDB.insert(data)
+        .then(newAction=>{
+            res.json(newAction)
+        })
+        .catch(err=>{
+            res.status(500).json({message: "problem with request"})
+        })
+})
+
+server.put('/actions/update/:id', (req, res)=>{
+    const { id } = req.params;
+    const data = req.body;
+    if(id){
+    actionDB.update(id, data)
+    .then(updated=>{
+        res.status(202).json(updated)
+    })
+    .catch(err=>{
+        res.status(500)
+            .json({ message: "Unable to update"})
+    })
+    }else{
+        res.status(404).json({message: "id not found"})
+    }
+})
+
+server.delete('/actions/remove/:id', (req,res)=>{
+    const { id } = req.params;
+    actionDB.remove(id)
+    .then(number=>{
+        number?res.json({message: "Action go Bye Bye"}):
+            res.status(400)
+                .json({message: "it might still be ther you should check"})
+    })
+    .catch(err=>{
+        res.status(500)
+            .json({message: "Trouble with removing this action"})
+    })
+})
+
 
 server.listen(PORT, ()=>{
     console.log(`Running on port: ${PORT}`)
